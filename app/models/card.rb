77033bc25b4d378ad86979fc81ee9920f84d746f
KosukeNozaki <kousuke920@gmail.com>
2020-12-19 09:51:58 +0000

    修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,6 @@ class Card < ApplicationRecord
 	attachment :image
 	has_many :cart_cards, dependent: :destroy
 	belongs_to :genre
-	belongs_to :order
 	has_many :order_items
 	def tax_price
 		price * 1.08
