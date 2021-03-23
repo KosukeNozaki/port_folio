6d7c1e3ee8a10f36561a5667ad6b35ed1c744896
KosukeNozaki <kousuke920@gmail.com>
2021-03-23 00:24:58 +0000

    ジャンルの修正

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,28 +1,28 @@
 class Admins::GenresController < ApplicationController
    # ジャンル管理画面（一覧・追加も含む）
   def index
-    genres = Genre.all
-    genre = Genre.new
+    @genres = Genre.all
+    @genre = Genre.new
   end
     # ジャンルデータ登録
   def create
-    genre = Genre.new(genre_params)
-    genre.save
+    @genre = Genre.new(genre_params)
+    @genre.save!
     redirect_to admins_genres_path
   end
     # ジャンル編集画面
   def edit
-    genre = Genre.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
     # ジャンルデータ更新
   def update
-    genre = Genre.find(params[:id])
-    genre.update(genre_params)
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
     redirect_to admins_genres_path
   end
   def destroy
-    genre = Genre.find(params[:id])
-    genre.destroy
+   @genre = Genre.find(params[:id])
+   @genre.destroy
     redirect_to admins_genres_path
   end
   private
